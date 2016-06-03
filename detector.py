18139239212f36fca1a4c625fe1d0794655c08ed
Eyal Ben-Simon <eyalben@gmail.com>
2016-06-03 15:25:20 +0300

    Initial design for client separation

---
+++ detector.py
@@ -0,0 +1,12 @@
+
+class Detection(object):
+    def __init__(self):
+        pass
+
+    def detect(self, buffer):
+        """
+        Returns a list of
+        :param buffer:
+        :return:
+        """
+        pass
--- pop3proxy.py
+++ pop3proxy.py
@@ -1,41 +1,6 @@
 import socket
 import select
-
-
-class Client(object):
-    def __init__(self, server, sock, addr, dest_host, dest_port):
-        self.server = server
-        self.sock = sock
-        self.addr = addr
-        self._dest_host = dest_host
-        self._dest_port = dest_port
-        self._connect_dest()
-
-        server.register(sock, self)
-        server.register(self.sock_pop3, self)
-
-    def _connect_dest(self):
-        sock_pop3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock_pop3.connect((self._dest_host, self._dest_port))
-        self.sock_pop3 = sock_pop3
-
-    def handle_closed(self):
-        self.server.unregister(self.sock)
-        self.server.unregister(self.sock_pop3)
-
-    def handle_ready(self, sock):
-        data = sock.recv(10000)
-
-        # Connection closed
-        if len(data) == 0:
-            self.handle_closed()
-
-        # Data came from client
-        if sock == self.sock:
-            self.sock_pop3.send(data)
-        # Data came from POP3 Server
-        else:
-            self.sock.send(data)
+from client import Client
 
 
 class POP3Proxy(object):
