00de39a836d9b907fab83a0676f77f94d78519ab
Eyal Ben-Simon <eyalben@gmail.com>
2016-06-28 16:30:35 +0300

    Added payload removal

--- detector.py
+++ detector.py
@@ -2,6 +2,8 @@ from hashlib import sha256
 
 
 class Detection(object):
+    error_message = ""
+
     def __init__(self):
         pass
 
@@ -10,6 +12,8 @@ class Detection(object):
 
 
 class Blacklist(Detection):
+    error_message = "File was blacklisted"
+
     def __init__(self, hashes):
         """
         :param hashes: List of SHA256 Hashes
@@ -18,6 +22,7 @@ class Blacklist(Detection):
 
     def is_malicious(self, file_content):
         file_hash = sha256(file_content).hexdigest()
+        print "$$ (%d) %r" % (len(file_content), file_content)
         if file_hash in self._hashes:
             return True
         return False
--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 from detector import Blacklist
 
 DETECTORS = [
-    Blacklist(["f82d950df3f9752bfd0b8227d37a4d8e5b3d9c148905d3b8f50d716f8519ba27"]),
+    Blacklist(["604ad1939c67866df4a3e05d30bc45cc6ec3de3403bd1b5c286a45d5b7564859"]),
               ]
